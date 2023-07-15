b9499b329b487de7cfddf86dd3cec89457c1b770
shelly-goldblit <shelly_goldblit@dell.com>
2023-07-15 12:20:23 +0300

    fix build

--- examples/react/pokemon-catalog/src/components/pokemon-catalog/pokemon-catalog.component.tsx
+++ examples/react/pokemon-catalog/src/components/pokemon-catalog/pokemon-catalog.component.tsx
@@ -52,7 +52,7 @@ export const PokemonCatalogComponent = (
   useEffect(() => {
     const getFirstPokemon = async () => fetchByOffset();
     getFirstPokemon();
-  }, [pokemonService]);
+  }, []);
 
   const getPokemonIndex = () =>
     Number(
