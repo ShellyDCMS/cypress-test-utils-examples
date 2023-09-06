44c5a455fd0d1bfc8032c9477b1a712538ae1c1a
shelly_goldblit <shelly_goldblit@dell.com>
2023-09-06 09:41:39 +0300

    add test suite name

--- examples/angular/pokemon-catalog/cypress/integration/pokemon-page.spec.cy.ts
+++ examples/angular/pokemon-catalog/cypress/integration/pokemon-page.spec.cy.ts
@@ -5,25 +5,27 @@ import { AppDriver } from "../drivers/app.driver";
 
 describe("Pokemon Page integration Tests", () => {
   const chance = new Chance();
-
   const { when, given, get, beforeAndAfter } = new AppDriver();
   beforeAndAfter();
-  const pokemonList: PokemonList = Builder<PokemonList>()
-    .results([{ name: chance.word(), url: "1" }])
-    .count(1)
-    .build();
 
-  beforeEach(() => {
-    given.pokemon.fetchPokemonResponse(pokemonList);
-    given.pokemon.fetchImageResponse("default.png");
-    when.visit("/");
-  });
+  describe("given a single pokemon", () => {
+    const pokemonList: PokemonList = Builder<PokemonList>()
+      .results([{ name: chance.word(), url: "1" }])
+      .count(1)
+      .build();
 
-  it("should disable next button once showing last pokemon", () => {
-    expect(get.pokemon.nextButton().should("be.disabled"));
-  });
+    beforeEach(() => {
+      given.pokemon.fetchPokemonResponse(pokemonList);
+      given.pokemon.fetchImageResponse("default.png");
+      when.visit("/");
+    });
 
-  it("should disable prev button once showing first pokemon", () => {
-    expect(get.pokemon.prevButton().should("be.disabled"));
+    it("should disable next button once showing last pokemon", () => {
+      expect(get.pokemon.nextButton().should("be.disabled"));
+    });
+
+    it("should disable prev button once showing first pokemon", () => {
+      expect(get.pokemon.prevButton().should("be.disabled"));
+    });
   });
 });
