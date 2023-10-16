1325d5e91f9fbbe5ddcec71843a1181169d9d9ed
shelly_goldblit <shelly_goldblit@dell.com>
2023-10-16 20:36:40 +0300

    revert tp 12.7.4

--- examples/react/pokemon-catalog/src/components/pokemon-catalog/pokemon-catalog.component.driver.tsx
+++ examples/react/pokemon-catalog/src/components/pokemon-catalog/pokemon-catalog.component.driver.tsx
@@ -1,5 +1,5 @@
-import { CypressHelper } from "@shellygo/cypress-test-utils";
-import { CypressReactComponentHelper } from "@shellygo/cypress-test-utils/react";
+import { CypressHelper } from "@shellygo/cypress-test-utils/dist/cypress-helper";
+import { CypressReactComponentHelper } from "@shellygo/cypress-test-utils/dist/react/cypress-react-component-helper";
 import { Attributes, ReactNode } from "react";
 import { IPokemonService, PokemonList } from "../../services/pokemon.service";
 import { PokemonImageComponentDriver } from "../pokemon-image/pokemon-image.component.driver";
