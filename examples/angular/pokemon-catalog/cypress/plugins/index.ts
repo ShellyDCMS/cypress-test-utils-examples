1c47a747300b832198e9264c2d1fb301b3db2f1f
shelly_goldblit <shelly_goldblit@dell.com>
2023-10-17 17:39:11 +0300

    maybe like so?

--- examples/angular/pokemon-catalog/cypress/plugins/index.ts
+++ examples/angular/pokemon-catalog/cypress/plugins/index.ts
@@ -4,5 +4,32 @@
 import * as registerCodeCoverageTasks from "@cypress/code-coverage/task";
 
 export default (on, config) => {
+  const webpackPreprocessor = require("@cypress/webpack-batteries-included-preprocessor");
+
+  const webpackOptions = webpackPreprocessor.defaultOptions.webpackOptions;
+
+  webpackOptions.module.rules.unshift({
+    test: /[/\\]@shellygo[/\\].+\.m?js$/,
+    resolve: {
+      fullySpecified: false
+    },
+    use: {
+      loader: "babel-loader",
+      options: {
+        plugins: ["@angular/compiler-cli/linker/babel"],
+        compact: false,
+        cacheDirectory: true
+      }
+    }
+  });
+
+  on(
+    "file:preprocessor",
+    webpackPreprocessor({
+      webpackOptions: webpackOptions,
+      typescript: require.resolve("typescript")
+    })
+  );
+
   return registerCodeCoverageTasks(on, config);
 };
